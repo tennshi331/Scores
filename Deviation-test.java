842066d800939217c0d75fb78e0b20a1034afafe
中里柊斗 <s.nakazato@third-scope.com>
2020-10-11 04:00:52 +0900

    マジ卍

--- Deviation-test.java
+++ Deviation-test.java
@@ -5,7 +5,7 @@ public class Deviation{
 		for(int i = 0; i < args.length; i = i + 2){
 			var[i+1] = Integer.parseInt(args[i+1]);
 			score[i/2] = new Score(args[i],var[i+1],0);
-			System.out.println(score[i].name + "の素点は" +  score[i].rawscore + "です。");
+			System.out.println(score[i/2].name + "の素点は" +  score[i/2].rawscore + "です。"); //[i/2]に入れたんだから[i/2]で参照しなきゃダメだよね！！
 		    }
 		}
 	}
--- Deviation.java
+++ Deviation.java
@@ -1,14 +1,16 @@
 public class Deviation{
 	public static void main(String[] args){
 		// create instances
-		int[] var = new int[args.length];
-		Score[] score = new Score[args.length];
+		int[] var = new int[args.length/2]; //素点の配列の要素数は8だよね！!
+		Score[] score = new Score[args.length/2]; //同じくだよね!!argsの長さは16もあるよね!!
 		for(int i = 0; i < args.length; i = i + 2){
-			var[i+1] = Integer.parseInt(args[i+1]);
+			var[i/2] = Integer.parseInt(args[i+1]); //var[i+1]だと奇数インデックスにだけ値が入ってるキショい配列になるよね!!
 			score[i/2] = new Score(args[i],var[i+1],0);
 		    }
 
 		// calculate average
+		//今回のエラーの核はここだね！！scoreをargs.length の長さで定義したせいでこのループが16回まわってたよね！!
+		//nullPointerExceptionは配列内で指定されたインデックスに要素が存在しないよ〜><ってやつだね！！エラー名でググろう
 		int sum = 0;
 		for(int i = 0; i < score.length; i++){
 			sum += score[i].rawscore;
@@ -19,9 +21,9 @@ public class Deviation{
 		int summultiplier = 0;
 		double sd = 0;
 		for(int i = 0; i < score.length; i++){
-			summultiplier += Math.pow(score[i].rawscore,2);
+			summultiplier += Math.pow(score[i].rawscore,2); //読みづらいから変数名が複数単語になるときはキャメルケースにするなどしようね！！ググってね！！
 			}
-		double averagemultiplier = summultiplier / score.length;
+		double averagemultiplier = summultiplier / score.length; //読みづらいから変s...
 		double distributed = averagemultiplier - Math.pow(average,2);
 		sd = Math.sqrt(distributed);
 
