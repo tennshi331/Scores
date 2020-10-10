b902cbe63e658d6ee955c52cbf949b2546a337c3
tennshi331 <67410031+tennshi331@users.noreply.github.com>
2020-10-10 16:10:31 +0900

    Update Score.java

--- Score.java
+++ Score.java
@@ -3,15 +3,12 @@ public class Score {
   String name;
   int rawscore;
   double deviation;
+
   // declaration of constructor
   public Score(String name, int rawscore,double deviation){
     this.name = name;
     this.rawscore = rawscore;
     this.deviation = deviation;
   }
-  // declaration of methods
-  @override
-  public String toString(){
-    return this.name + "の偏差値は" + this.deviation + "です。";
-  }
 }
+
