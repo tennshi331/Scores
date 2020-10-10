62a365e44055b2632a980003444262b8629cebf6
tennshi331 <67410031+tennshi331@users.noreply.github.com>
2020-10-10 16:10:11 +0900

    Update Deviation.java

--- Deviation.java
+++ Deviation.java
@@ -1,32 +1,33 @@
 public class Deviation{
-  public static void main(String[] args){
-    for(int i = 0; i < args.length; i = i + 2){
-      Score[] score = {
-        new Score(args[i],args[i+1],"")
-        };
-    }
-    
-    // caluculate average
-    int sum = 0;
-    double sd = 0.0;
-    for(int i = 0; i < score.length; i++){
-      sum += score[i].rawscore;
-    }
-    double average = sum / score.length;
-    
-    // caluculate standard deviation
-    int summultiplier = 0;
-    for(int i = 0; i < score.length; i++){
-      summultiplier += Math.pow(score[i].rawscore,2);
-    }
-    double averagemultiplier = summultiplier / score.length;
-    double distributed = averagemultiplier - Math.pow(average,2);
-    double sd = Math.sqrt(distributed);
-    
-    // caluculate deviation and express
-    for(int i = 0; i < score.length; i++){
-      score[i].deviation = (score[i].rawscore - average) / sd * 10 + 50;
-      System.out.println(score[i].name + "の偏差値は" +  score[i].deviation + "です。");
-    }
-  }
-}
+	public static void main(String[] args){
+		int[] var = new int[args.length];
+		Score[] score = new Score[args.length];
+		for(int i = 0; i < args.length; i = i + 2){
+			var[i+1] = Integer.parseInt(args[i+1]);
+			score[i/2] = new Score(args[i],var[i+1],0);
+		    }
+
+		// calculate average
+		int sum = 0;
+		for(int i = 0; i < score.length; i++){
+			sum += score[i].rawscore;
+			}
+		double average = sum / score.length;
+
+		// calculate standard deviation
+		int summultiplier = 0;
+		double sd = 0;
+		for(int i = 0; i < score.length; i++){
+			summultiplier += Math.pow(score[i].rawscore,2);
+			}
+		double averagemultiplier = summultiplier / score.length;
+		double distributed = averagemultiplier - Math.pow(average,2);
+		sd = Math.sqrt(distributed);
+
+		// calculate deviation and express
+		for(int i = 0; i < score.length; i++){
+			score[i].deviation = (score[i].rawscore - average) / sd * 10 + 50;
+			System.out.println(score[i].name + "の偏差値は" +  score[i].deviation + "です。");
+			}
+		}
+	}
