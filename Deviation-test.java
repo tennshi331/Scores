02298fa71c51be4b9a4687561919ffc8f0f9e9aa
Shuto Nakazato <41186761+ShutoNakazato@users.noreply.github.com>
2020-10-11 16:51:02 +0900

    マジマジ卍卍卍

--- Deviation-test.java
+++ Deviation-test.java
@@ -1,7 +1,7 @@
 public class Deviation{
 	public static void main(String[] args){
-		int[] var = new int[args.length];  //素点の配列の要素数は8だよね！!半分にしよう!!
-		Score[] score = new Score[args.length];  //同じくだよね!!argsの長さは16もあるよね!!
+		int[] var = new int[args.length/2];  //素点の配列の要素数は8だよね！!半分にしよう!!
+		Score[] score = new Score[args.length/2];  //同じくだよね!!argsの長さは16もあるよね!!
 		for(int i = 0; i < args.length; i = i + 2){
 			var[i+1] = Integer.parseInt(args[i+1]);  //var[i+1]だと奇数インデックスにだけ値が入ってるキショい配列になるよね!!
 			score[i/2] = new Score(args[i],var[i+1],0);
